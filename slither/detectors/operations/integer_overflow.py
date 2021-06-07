27d1a70bfaeb9a9bd2f23cd12eabde9dab5d9dbb
Hypocrisy141 <81597671+Hypocrisy141@users.noreply.github.com>
2021-06-07 15:55:28 +0800

    Add files via upload

---
+++ slither/detectors/operations/integer_overflow.py
@@ -0,0 +1,36 @@
+from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+
+
+class Integer_overflow(AbstractDetector):
+    """
+    Detect Integer_overflow
+    """
+
+    ARGUMENT = "integer_overflow"  # slither will launch the detector with slither.py --mydetector
+    HELP = "integer_overflow (detector example)"
+    IMPACT = DetectorClassification.HIGH
+    CONFIDENCE = DetectorClassification.HIGH
+
+    WIKI = "https://github.com/trailofbits/slither/wiki/Adding-a-new-detector"
+    WIKI_TITLE = "Integer_overflow example"
+    WIKI_DESCRIPTION = "integer_overflow example"
+    WIKI_EXPLOIT_SCENARIO = ".."
+    WIKI_RECOMMENDATION = ".."
+
+    def _detect(self):
+        results = []
+
+        for contract in self.slither.contracts_derived:
+            # Check if a function has 'backdoor' in its name
+            for f in contract.functions:
+                if "safemath.sol" not in f.data:
+                    if "+" in f.data or "-" in f.data or "*" in f.data:
+                        # Info to be printed
+                        info = ["integer_overflow found in ", f, "\n"]
+
+                        # Add the result in result
+                        res = self.generate_result(info)
+
+                        results.append(res)
+
+        return results
